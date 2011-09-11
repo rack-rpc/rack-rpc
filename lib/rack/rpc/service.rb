b7fe1783c5fa20aa466dfded48bd30bac4914516
Arto Bendiken <arto@bendiken.net>
2011-09-11 02:41:11 +0200

    Implemented the Service.operators and Service[] class methods.

--- lib/rack/rpc/service.rb
+++ lib/rack/rpc/service.rb
@@ -19,8 +19,28 @@ module Rack::RPC
     # @return [void]
     def self.operator(klass, options = {})
       raise TypeError, "expected a Class, but got #{klass.inspect}" unless klass.is_a?(Class)
+      operators[klass] ||= options
+    end
+
+    ##
+    # Returns the operator definitions for this service class.
+    #
+    # @return [Hash{Class => Hash}]
+    def self.operators
       @operators ||= {}
-      @operators[klass] ||= options
+    end
+
+    ##
+    # Returns the operator class for the given operator name.
+    #
+    # @param  [Symbol, #to_sym] operator_name
+    # @return [Class]
+    def self.[](operator_name)
+      operator_name = operator_name.to_sym
+      operators.find do |klass, options|
+        klass_name = klass.name.split('::').last # TODO: optimize this
+        return klass if operator_name.eql?(klass_name.to_sym)
+      end
     end
   end # Service
 end # Rack::RPC
